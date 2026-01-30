737641a841178ae7af16e0eb29af01af0db36cfd
Geco Games Studios <gecogameslimited@gmail.com>
2026-01-30 11:16:19 +0200

    Patch 0.0.9

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+//  GNU nano 6.2                                             ecosystem.config.js
+module.exports = {
+  apps: [{
+    name: 'mifi-landing',
+    script: 'npm',
+    args: 'start',
+    cwd: '/root/system/mifi-landing',  // Explicit working directory
+    instances: 1,
+    exec_mode: 'fork',            // Use 'fork' for Next.js
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    error_file: '/root/.pm2/logs/mifi-landingerror.log',
+    out_file: '/root/.pm2/logs/mifi-landing-out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3001
+    },
+    env_development: {
+      NODE_ENV: 'development',
+      PORT: 3001
+    }
+  }]
+};
+
+
+
+
+
+                                                          
